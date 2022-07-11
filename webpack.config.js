8194bb7b50e4727fdcf9b331a94c53c9132782e4
oseiGyapong <oseigyapong@logig.tech>
2022-07-11 05:02:25 +0000

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: path.resolve(__dirname, 'src/index.ts')
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].[contenthash].bundle.js',
+        clean: true,
+        assetModuleFilename: '[name][ext]'
+    },
+    devtools: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'public')
+        },
+        port: 3000,
+        open: false,
+        hot: true,
+        compress: true,
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                },
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'assets/images'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Web Component ToDo App',
+            filename: 'index.html',
+            template: 'src/template.html'
+        })
+    ]
+}
